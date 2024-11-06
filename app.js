1286a063f2b91740ea99418451f59b9fe249c690
Isabelle Lopes <isabelle.biomed@gmail.com>
2024-11-06 20:05:25 -0300

    adc link

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const updateProductPrice = document.querySelector('#update-price');
 
 // Function to fetch all products from the server
 async function fetchProducts() {
-  const response = await fetch('http://localhost:3000/products');
+  const response = await fetch('http://34.224.21.89:3000/products');
   const products = await response.json();
 
   // Clear product list
